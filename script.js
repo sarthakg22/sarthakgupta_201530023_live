60cbb5ad911b6d11afe401edcf451b85654d22ba
sarthakg22 <109089045+sarthakg22@users.noreply.github.com>
2022-07-11 20:17:32 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+var n=document.createElement("span");
+n.innerHTML="Anmol Pandey";
+document.getElementById("js").appendChild(n);
+
+var roll=document.createElement("span");
+roll.innerHTML="201530005";
+document.getElementById("js").appendChild(roll);
+
+var coll=document.createElement("span");
+coll.innerHTML="GLA University";
+document.getElementById("js").appendChild(coll);
+
+var date=document.createElement("span");
+date.innerHTML="11-07-2022";
+document.getElementById("js").appendChild(date);
